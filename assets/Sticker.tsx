e23e790a038334a16f9a0be820d626d35abb5a3d
png261 <nhphuong.code@gmail.com>
2023-07-19 09:16:43 +0700

    feat: load template object tiled map

---
+++ assets/Sticker.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Sticker" tilewidth="17" tileheight="26" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="bombup-sticker.png"/>
+ </tile>
+ <tile id="1">
+  <image width="17" height="26" source="bulb-sticker.png"/>
+ </tile>
+ <tile id="2">
+  <image width="13" height="24" source="bomberman-sticker.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="flameup-sticker.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="speedup-sticker.png"/>
+ </tile>
+ <tile id="5">
+  <image width="15" height="26" source="balloom-sticker.png"/>
+ </tile>
+</tileset>
