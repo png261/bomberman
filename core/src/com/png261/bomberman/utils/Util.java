129dc6cb6949958a70448f21b559a2bf40250ef6
png261 <nhphuong.code@gmail.com>
2023-07-30 10:37:33 +0700

    chore: remove unused function

--- core/src/com/png261/bomberman/utils/Util.java
+++ core/src/com/png261/bomberman/utils/Util.java
@@ -4,11 +4,4 @@ public final class Util {
 	public static int getRandomInRange(int min, int max) {
 		return (int) ((Math.random() * (max - min)) + min);
 	}
-
-	public static short orOperation(short... bits) {
-		short res = 0;
-		for (short bit : bits)
-			res |= bit;
-		return res;
-	}
 }
